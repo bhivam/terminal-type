25a1657ffad62df705e2eae39cfe3a9600f756f8
bhivam <shivamkajaria@gmail.com>
2022-11-01 15:21:32 -0400

    Added in start button

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <unistd.h>
@@ -80,9 +81,30 @@ char editorProcessKeypress()
 void editorRefreshScreen()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
-char **wordBank()
+int randint(int n) 
+{
+    if ((n-1) == RAND_MAX) {
+        return rand();
+    }
+    else 
+    {
+        // The reason why there would be a skew if the just did rand() % n is that if RAND_MAX was say 13 and n was 4, then 0 and 1 would have one more occurance than 2 and 3. This skews the randomness.
+        // by finding where the rand_max should be according to our n, we can generate a less skewed example by throwing out anything above the correct rand_max.
+        int end = RAND_MAX / n;
+        end = end * n;
+        
+        int r;
+        while ((r = rand()) >= end);
+
+        return r % n;
+
+    }
+}
+
+char *wordBank()
 {
     // get number of words first
     int n = 0;
@@ -95,11 +117,11 @@ char **wordBank()
     int largestWordLen = 0;
     int currentWordLen = 0;
     int numWords = 0;
-    char lastChar = '\0';
+    char lastChar = ' ';
 
     while ((n = read(fd, &c, 1)) == 1)
     {
-        if (n == ' ' || n == '\n')
+        if (c == ' ' || c == '\n')
         {
             if (currentWordLen > largestWordLen)
                 largestWordLen = currentWordLen;
@@ -112,31 +134,90 @@ char **wordBank()
             currentWordLen++;
         }
 
-        write(STDOUT_FILENO, &c, 1);
-
-        lastChar = n;
+        lastChar = c;
     }
 
     if (lastChar != ' ' && lastChar != '\n')
         numWords++;
 
-    write(STDOUT_FILENO, "\r\n", 2);
+    char **words = malloc(sizeof(char *) * numWords);
+    for (int i = 0; i < numWords; i++) 
+    {
+        words[i] = malloc(sizeof(char) * (largestWordLen + 1));
+    }
 
-    return 0;
+    fd = open("../data/word_bank.txt", O_RDONLY);
+    if (fd == -1) 
+        die("open");
+    
+    int j = 0;
+    int k = 0;
+    while ((n = read(fd, &c, 1)) == 1 && j < numWords) 
+    {
+        if (c == ' ' || c == '\n') 
+        {
+            words[j][k] = '\0';
+            j++;
+            k = 0;
+        }
+        else 
+        {
+            words[j][k] = c;
+            k++;
+        }
+    }
+
+    char *phrase = malloc((1 + 30 * (largestWordLen + 1)) * sizeof(char));
+    int phraseIdx = 0;
+
+    for (int i = 0; i < 30; i++) 
+    {
+        int randIdx = randint(numWords);
+        for (char *t = words[randIdx]; *t != '\0'; t++)
+        {
+            phrase[phraseIdx] = *t;
+            phraseIdx++;
+        }
+
+        if (i != 29)
+        {
+            phrase[phraseIdx] = ' ';
+            phraseIdx++;
+        }
+    }
+
+    phrase[phraseIdx] = '\0';
+
+    for (int i = 0; i < numWords; i++)
+    {
+        free(words[i]);
+    }
+    free(words);
+
+    return phrase;
 }
 
 int main()
 {
     enableRawMode();
+    editorRefreshScreen();
 
-    wordBank();
-
-    char *phrase = "type this phrase to test typing";
+    char *phrase = wordBank();
     int phraseIdx = 0;
+
+    char *instruction = "Press Space to Continue";
 
     char lastChar = '\0';
 
     printf("%s\r\n\n", phrase);
+
+    write(STDOUT_FILENO, instruction, strlen(instruction));
+
+    while (editorProcessKeypress() != ' ');
+
+    write(STDOUT_FILENO, "\x1b[2K", 4);
+
+    write(STDOUT_FILENO, "\r", 1);
 
     clock_t t;
     t = clock();
@@ -147,10 +228,11 @@ int main()
             break;
 
         lastChar = editorProcessKeypress();
-
+    
         if (lastChar != 0)
         {
             write(STDOUT_FILENO, "\x1b[30m", 5);
+            
             if (phrase[phraseIdx] == lastChar)
             {
                 phraseIdx++;
@@ -171,9 +253,11 @@ int main()
     printf("\r\n\n");
 
     t = clock() - t;
-    double wpm = 6 / ((((double)t) / CLOCKS_PER_SEC) * 60);
+    double wpm = 30 / ((((double)t) / CLOCKS_PER_SEC) * 60);
 
     printf("wpm: %lf", wpm);
+
+    free(phrase);
 
     return 0;
 }
