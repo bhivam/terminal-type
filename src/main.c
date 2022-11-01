d9d5b69a7c7ab794dfe4fd57e3828b65a8ac7c7a
bhivam <shivamkajaria@gmail.com>
2022-11-01 01:31:37 -0400

    Get max word length and number of words.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <ctype.h>  
+#include <ctype.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -7,11 +7,12 @@
 #include <time.h>
 #include <fcntl.h>
 
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k)&0x1f)
 
 struct termios orig_termios;
 
-void die (const char *s) {
+void die(const char *s)
+{
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
 
@@ -19,12 +20,16 @@ void die (const char *s) {
     exit(1);
 }
 
-void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+void disableRawMode()
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
-void enableRawMode() {
-    if ( tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+void enableRawMode()
+{
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -36,81 +41,123 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
 
-char editorReadKey() {
+char editorReadKey()
+{
     char c;
-    int nread; 
+    int nread;
 
     // This will read in a new number. If read() does not read in 1 byte, we check for an error. Once nread
-    // places 1 byte in c, the loop exits and c is returned. 
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1) die("read");
+    // places 1 byte in c, the loop exits and c is returned.
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1)
+            die("read");
     }
-    
+
     return c;
 }
 
-char editorProcessKeypress() {
+char editorProcessKeypress()
+{
     char c = editorReadKey();
 
-    switch (c) {
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            exit(0);
-            break;
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
     }
 
     return c;
 }
 
-void editorRefreshScreen() {
+void editorRefreshScreen()
+{
     write(STDOUT_FILENO, "\x1b[2J", 4);
 }
 
-char **wordBank() {
+char **wordBank()
+{
     // get number of words first
     int n = 0;
     char c;
     int fd = open("../data/word_bank.txt", O_RDONLY);
 
-    if (fd == -1) die("open");
+    if (fd == -1)
+        die("open");
 
-    while ((n = read(fd, &c, 1)) == 1) {
+    int largestWordLen = 0;
+    int currentWordLen = 0;
+    int numWords = 0;
+    char lastChar = '\0';
+
+    while ((n = read(fd, &c, 1)) == 1)
+    {
+        if (n == ' ' || n == '\n')
+        {
+            if (currentWordLen > largestWordLen)
+                largestWordLen = currentWordLen;
+            currentWordLen = 0;
+        }
+        else
+        {
+            if (lastChar == ' ' || lastChar == '\n')
+                numWords++;
+            currentWordLen++;
+        }
+
         write(STDOUT_FILENO, &c, 1);
+
+        lastChar = n;
     }
-    
+
+    if (lastChar != ' ' && lastChar != '\n')
+        numWords++;
+
+    write(STDOUT_FILENO, "\r\n", 2);
+
     return 0;
 }
 
-int main() {
+int main()
+{
     enableRawMode();
 
     wordBank();
-    
+
     char *phrase = "type this phrase to test typing";
     int phraseIdx = 0;
 
     char lastChar = '\0';
 
     printf("%s\r\n\n", phrase);
-    
+
     clock_t t;
     t = clock();
 
-    while (1) {
-        if (phrase[phraseIdx] == '\0') break;
-        
+    while (1)
+    {
+        if (phrase[phraseIdx] == '\0')
+            break;
+
         lastChar = editorProcessKeypress();
 
-        if (lastChar != 0) {
+        if (lastChar != 0)
+        {
             write(STDOUT_FILENO, "\x1b[30m", 5);
-            if (phrase[phraseIdx] == lastChar) {
+            if (phrase[phraseIdx] == lastChar)
+            {
                 phraseIdx++;
                 write(STDOUT_FILENO, "\x1b[42m", 5);
-            } else {
+            }
+            else
+            {
                 write(STDOUT_FILENO, "\x1b[41m", 5);
             }
 
@@ -124,10 +171,9 @@ int main() {
     printf("\r\n\n");
 
     t = clock() - t;
-    double wpm = 6 / ((((double)t)/CLOCKS_PER_SEC) * 60);
+    double wpm = 6 / ((((double)t) / CLOCKS_PER_SEC) * 60);
 
     printf("wpm: %lf", wpm);
 
     return 0;
 }
-
